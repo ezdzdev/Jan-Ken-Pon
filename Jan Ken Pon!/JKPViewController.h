7a19c65a1348e1d81730c0d383e85786a2ff8ce8
EzGame <zyq.david@gmail.com>
2014-07-04 15:36:33 -0400

    Basic JKP logic completed. - Random CPU - Score

--- Jan Ken Pon!/JKPViewController.h
+++ Jan Ken Pon!/JKPViewController.h
@@ -9,5 +9,14 @@
 #import <UIKit/UIKit.h>
 
 @interface JKPViewController : UIViewController
+- (IBAction)scissorsTouched:(id)sender;
+- (IBAction)paperTouched:(id)sender;
+- (IBAction)rockTouched:(id)sender;
 
+@property (weak, nonatomic) IBOutlet UILabel *scoreLabel;
+@property (weak, nonatomic) IBOutlet UILabel *playerChoiceLabel;
+@property (weak, nonatomic) IBOutlet UILabel *computerChoiceLabel;
+@property (weak, nonatomic) IBOutlet UIButton *scissorsButton;
+@property (weak, nonatomic) IBOutlet UIButton *paperButton;
+@property (weak, nonatomic) IBOutlet UIButton *rockButton;
 @end
--- Jan Ken Pon!/JKPViewController.m
+++ Jan Ken Pon!/JKPViewController.m
@@ -8,8 +8,21 @@
 
 #import "JKPViewController.h"
 
-@interface JKPViewController ()
+typedef enum {
+    ROCK = 1,
+    PAPER = 2,
+    SCISSORS = 3,
+} JKPChoice;
 
+typedef enum {
+    WIN = 1,
+    TIE = 0,
+    LOSS = -1,
+} JKPResult;
+
+@interface JKPViewController () {
+    int score;
+}
 @end
 
 @implementation JKPViewController
@@ -17,7 +30,9 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
 	// Do any additional setup after loading the view, typically from a nib.
+    score = 0;
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +41,71 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)scissorsTouched:(id)sender {
+    self.playerChoiceLabel.text = @"Scissors!";
+    [self gameVersesRandomCPU:SCISSORS];
+}
+
+- (IBAction)paperTouched:(id)sender {
+    self.playerChoiceLabel.text = @"Paper!";
+    [self gameVersesRandomCPU:PAPER];
+}
+
+- (IBAction)rockTouched:(id)sender {
+    self.playerChoiceLabel.text = @"Rock!";
+    [self gameVersesRandomCPU:ROCK];
+}
+
+
+
+
+
+#pragma mark - Game Logic
+- (void) gameVersesRandomCPU:(JKPChoice)playerChoice {
+    // Random CPU, choses between 1-3 using rand()
+    JKPChoice randomChoice = (int)rand()%3 + 1;
+
+    if ( randomChoice == ROCK )
+        self.computerChoiceLabel.text = @"Rock!";
+    else if ( randomChoice == PAPER )
+        self.computerChoiceLabel.text = @"Paper!";
+    else if ( randomChoice == SCISSORS )
+        self.computerChoiceLabel.text = @"Scissors!";
+    
+    // Find results
+    JKPResult result = [self A:playerChoice verseB:randomChoice];
+    
+    if ( result == WIN )
+        self.view.backgroundColor = [UIColor greenColor];
+    else if ( result == LOSS )
+        self.view.backgroundColor = [UIColor redColor];
+    else
+        self.view.backgroundColor = [UIColor yellowColor];
+    
+    score += result;
+    [self updateScore];
+}
+
+- (JKPResult) A:(JKPChoice)a verseB:(JKPChoice)b {
+    NSAssert(!(a < 1 || a > 3), @"CHOICE A OUT OF BOUNDS [%d]", a);
+    NSAssert(!(b < 1 || b > 3), @"CHOICE B OUT OF BOUNDS [%d]", b);
+    
+    // Tie state
+    if ( a == b ) return TIE;
+    
+    // Win state
+    if ( a == PAPER && b == ROCK )
+        return WIN;
+    else if ( a == ROCK && b == SCISSORS)
+        return WIN;
+    else if ( a == SCISSORS && b == PAPER)
+        return WIN;
+    
+    // Loss state
+    return LOSS;
+}
+
+- (void) updateScore {
+    self.scoreLabel.text = [NSString stringWithFormat:@"Score: %d", score];
+}
 @end
